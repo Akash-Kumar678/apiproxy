51e0c42f00e5fe1e6d3b62c018793db0a49ecc09
Akash Kumar <90201774+Akash-Kumar678@users.noreply.github.com>
2022-07-18 10:59:42 +0530

    api proxy

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+const app = express()
+import fetch from 'node-fetch'
+
+app.get('/search/:userQuery', async (req,res)=>{
+   const userQuery = req.params.userQuery
+    const url  = `https://newsapi.org/v2/everything?q=${userQuery}&apiKey=e231658e9222455abd52a3d89d4c126b`
+   const response = await fetch(url)
+   const json = await response.json()
+   const data = json.articles.map(item=>item)
+    res.send(data)
+})
+
+app.get('/top-headlines', async (req,res)=>{
+   const url  = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e231658e9222455abd52a3d89d4c126b`
+    const response = await fetch(url)
+    const json = await response.json()
+    const data = json.articles.map(item=>item)
+    res.send(data)
+ })
+
+ app.get('/bbc', async (req,res)=>{
+   
+     const url  = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=e231658e9222455abd52a3d89d4c126b`
+    const response = await fetch(url)
+    const json = await response.json()
+    const data = json.articles.map(item=>item)
+    res.json(json)
+ res.send(data)
+ })
+
+ app.get('/category/:categoryName',async (req,res)=>{
+    const categoryName = req.params.categoryName
+     const url  = `https://newsapi.org/v2/top-headlines?category=${categoryName}&apiKey=e231658e9222455abd52a3d89d4c126b`
+    const response = await fetch(url)
+    const json = await response.json()
+    const data = json.articles.map(item=>item)
+    res.send(data)
+ })
+
+
+app.listen(8000,()=>{
+    console.log('poost running : 8000')
+})
