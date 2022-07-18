b9dfaa5f37b99e65c016b92a0fa09f678fc5397b
Akash Kumar <90201774+Akash-Kumar678@users.noreply.github.com>
2022-07-18 11:10:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.get('/top-headlines', async (req,res)=>{
  })
 
 
-app.listen(process.env.PORT || 8000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log('poost running : 8000')
 })
